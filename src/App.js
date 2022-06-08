f04f046d9925d61447100111384a0e5998417968
LDuszynski <leszekduszynski@gmail.com>
2022-06-08 19:34:29 +0200

    0.5a get cars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,3 @@
-// zainstalowac npm install react-router-dom@6
-//              npm install axios
-
 //import logo from './logo.svg';
 //import './App.css';
 import React, { useCallback, useState } from 'react';
--- src/components/ReactCardSlider.jsx
+++ src/components/ReactCardSlider.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useCallback, useState } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import './Slider.css';
+import axios from "axios";
+
 const ReactCardSlider = (props) => {
+
+    const [cars, setCars] = useState([])
+
+    const componentDidMount = () => {
+        axios.get(`http://localhost:8080/file`)
+          .then(res => {
+            const cars = res.data;
+            this.setState({ cars });
+            console.log("mamy to")
+        })
+    }
+
+    const fetchData = useCallback(async () => {
+        try {
+            const result = await axios.get(`http://localhost:8080/file`)
+            //const result = await authAxios.get(`/car/models`)
+            setCars(result.data)
+            console.log("mamy to")
+        } catch (err) {
+            setRequestError(err.message);
+        }
+
+    })
+
+
     const slides = [{ image: 'https://picsum.photos/200/300', title: "this is a title", description: "This is description analiza" },
         { image: 'https://picsum.photos/200/300', title: "this is a title", description: "This is description analiza" },
         { image: 'https://picsum.photos/200/400', title: "this is a title", description: "This is description analiza" },
@@ -27,20 +54,24 @@ const ReactCardSlider = (props) => {
 
     return (
         <div id="main-slider-container">
-        <MdChevronLeft size={40} className="slider-icon left" onClick={ slideLeft}/>
-        <div id='slider'>
             {
-                slides.map((slide, index) => {
-                    return (
-                        <div className="slider-card" key={index}>
-                            <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover'}}> </div>
-                            <p className="slider-card-title">{slide.title}</p>
-                            <p className="slider-card-description">{slide.description}</p>
-                        </div>
-                    )
-                })}
-        </div>
-        <MdChevronRight size={40} className="slider-icon right" onClick={slideRight}/>
+                componentDidMount()
+                //fetchData()
+            }
+            <MdChevronLeft size={40} className="slider-icon left" onClick={ slideLeft}/>
+            <div id='slider'>
+                {
+                    slides.map((slide, index) => {
+                        return (
+                            <div className="slider-card" key={index}>
+                                <div className="slider-card-image" style={{ backgroundImage: `url(${slide.image})`, backgroundSize: 'cover'}}> </div>
+                                <p className="slider-card-title">{slide.title}</p>
+                                <p className="slider-card-description">{slide.description}</p>
+                            </div>
+                        )
+                    })}
+            </div>
+            <MdChevronRight size={40} className="slider-icon right" onClick={slideRight}/>
         </div>
         )
 }
